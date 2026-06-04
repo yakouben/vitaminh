b7fbd6c167540b723b2c5cd77b424bb148947504
Yakoub Bensalah <158473105+yakouben@users.noreply.github.com>
2026-06-04 17:35:57 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,21 +4,20 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "VITAMINH Lagree Body Lab - Thao Dien, Ho Chi Minh City",
   description:
-    "Reformer Pilates I Yoga I Bootcamp - HERSPACE is a 400sqm brand new premium studio in the city centre of Munich, designed exclusively by women for women.",
+    "Reformer Pilates I Yoga I Bootcamp - VITAMINH is a 400sqm brand new premium studio in the city centre of Munich, designed exclusively by women for women.",
   openGraph: {
     title: "VITAMINH Lagree Body Lab - Thao Dien, Ho Chi Minh City",
     description:
-      "Reformer Pilates I Yoga I Bootcamp - HERSPACE is a 400sqm brand new premium studio in the city centre of Munich, designed exclusively by women for women.",
-    url: "https://herspace.studio/",
-    siteName: "HERSPACE Studio",
+      "Reformer Pilates I Yoga I Bootcamp - VITAMINH is a 400sqm brand new premium studio in the city centre of Munich, designed exclusively by women for women.",
+    siteName: "VITAMINH Studio",
     type: "website",
     locale: "en_US",
   },
   twitter: {
     card: "summary_large_image",
-    title: "REFORMER PILATES I YOGA I BOOTCAMP - Herspace Studio",
+    title: "REFORMER PILATES I YOGA I BOOTCAMP - VITAMINH Studio",
     description:
-      "Reformer Pilates I Yoga I Bootcamp - HERSPACE is a 400sqm brand new premium studio in the city centre of Munich, designed exclusively by women for women.",
+      "Reformer Pilates I Yoga I Bootcamp - VITAMINH is a 400sqm brand new premium studio in the city centre of Munich, designed exclusively by women for women.",
   },
 };
 
